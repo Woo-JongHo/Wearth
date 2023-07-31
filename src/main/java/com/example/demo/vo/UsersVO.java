76ed9fb8fd455280d92e642984f16ace7aff6d84
jiyoung park <miracle48693@naver.com>
2023-07-31 17:55:00 +0900

    userVO-setter,getter 수정 footer-width 조절

--- src/main/java/com/example/demo/vo/UsersVO.java
+++ src/main/java/com/example/demo/vo/UsersVO.java
@@ -10,8 +10,7 @@ import lombok.Data;
 import lombok.ToString;
 
 @Entity
-@Data
-@ToString
+
 @Table(name="users")
 public class UsersVO {
 	@Id
@@ -29,4 +28,90 @@ public class UsersVO {
 	private int point;
 	private String u_status;
 	private String role;
+	public int getUserno() {
+		return userno;
+	}
+	public void setUserno(int userno) {
+		this.userno = userno;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getU_name() {
+		return u_name;
+	}
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+	public LocalDate getDate_birth() {
+		return date_birth;
+	}
+	public void setDate_birth(LocalDate date_birth) {
+		this.date_birth = date_birth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public LocalDate getDate_reg() {
+		return date_reg;
+	}
+	public void setDate_reg(LocalDate date_reg) {
+		this.date_reg = date_reg;
+	}
+	public String getResidence() {
+		return residence;
+	}
+	public void setResidence(String residence) {
+		this.residence = residence;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getU_status() {
+		return u_status;
+	}
+	public void setU_status(String u_status) {
+		this.u_status = u_status;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
 }
